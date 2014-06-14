55463d945bce500c28ad9ac9dbbfff0bf8ef42a4
Mark Jordan <mjordan@sfu.ca>
2014-06-14 10:12:26 -0700

    Added placeholder display of datastream; need to generalize the code to handle any DSID.

--- theme/item.tpl.php
+++ theme/item.tpl.php
@@ -3,17 +3,14 @@
  * @file
  * This is the template file for the default Islandora Feeds object page.
  */
-$islandora_content = $islandora_object['DATA']->content;
 ?>
-
 <div class="islandora-feeds-object islandora">
-  <div class ="feeds_thumb">
-    <?php print $variables['islandora_thumbnail_img'] ?>
-  </div>
-  <div class="islandora-basic-image-content-wrapper clearfix">
+  <div class="islandora-feeds-content-wrapper clearfix">
     <?php if (isset($islandora_content)): ?>
       <div class="islandora-feeds-content">
+        <pre>
         <?php print $islandora_content; ?>
+        </pre>
       </div>
     <?php endif; ?>
   </div>
