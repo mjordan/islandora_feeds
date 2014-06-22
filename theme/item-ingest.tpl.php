c79e730d05fc769bbabd25f073a71c73724aab12
Mark Jordan <mjordan@sfu.ca>
2014-06-22 16:42:03 -0700

    Migrated generation of OBJ XML to theming layer.

---
+++ theme/item-ingest.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * This is the ingest template file for the default Islandora Feeds object page.
+ */
+?>
+<?php print '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
+<fielddata>
+ <title><?php print $title; ?></title>
+ <field_1><?php print $field_the_second_field; ?></field_1>
+ <field_2><?php print $field_the_third_field; ?></field_2>
+</fielddata>
