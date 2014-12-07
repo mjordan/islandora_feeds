3451d2cdad0a7064bc4ee7846cdb735c1012cae2
Mark Jordan <mjordan@sfu.ca>
2014-12-06 16:50:15 -0800

    Removed debugging cruft.

--- theme/item-ingest.tpl.php
+++ theme/item-ingest.tpl.php
@@ -8,10 +8,7 @@
 <fielddata>
  <title label="Title"><?php print $title; ?></title>
  <?php foreach($variables['field_data'] as $field_data): ?>
-   <?php if(isset($field_data['field_name'])): ?>
-     <?php dd($field_data, "Field from within template"); ?>
      <<?php print $field_data['field_name']; ?> label="<?php print $field_data['field_label']; ?>"><?php print $field_data['field_value']; ?></<?php print $field_data['field_name'];?>>
-     <?php endif; ?>
  <?php endforeach; ?>
 </fielddata>
 
